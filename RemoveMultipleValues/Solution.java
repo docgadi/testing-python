739748670faa1c0690fa58d1b2b927c71ab69ab2
1slewis <77329743+1slewis@users.noreply.github.com>
2022-05-16 21:55:43 +0100

    Create Solution.java

---
+++ RemoveMultipleValues/Solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public static int[] solution(int[] data, int n) {
+        // Your code here
+        ArrayList<Integer> dataArrayList = new ArrayList<Integer>();
+        ArrayList<Integer> removeNums = new ArrayList<Integer>();
+        for (int i : data) {
+            dataArrayList.add(i);
+        }
+        for (int i = 0; i < dataArrayList.size(); i++) {
+            int amount = 0;
+            for (int j = 0; j < dataArrayList.size(); j++) {
+                if (dataArrayList.get(j).equals(dataArrayList.get(i))) {
+                    amount++;
+                }
+            }
+            if (amount > n) {
+                removeNums.add(dataArrayList.get(i));
+            }
+        }
+        for (int i = 0; i < removeNums.size(); i++) {
+            for (int j = 0; j < dataArrayList.size(); j++) {
+                if (dataArrayList.get(j).equals(removeNums.get(i))) {
+                    dataArrayList.remove(j);
+                    break;
+                }
+            }
+        }
+        int[] output = new int[dataArrayList.size()];
+        for (int i = 0; i < dataArrayList.size(); i++) {
+            output[i] = dataArrayList.get(i);
+        }
+        return output;
+    }
+}
